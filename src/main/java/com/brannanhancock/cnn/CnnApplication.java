efb4967275c31b13cd97fd67a88df268b6109ca2
BrannanH <brannanhancock@gmail.com>
2018-09-08 21:51:13 +0100

    adding initial setup

---
+++ src/main/java/com/brannanhancock/cnn/CnnApplication.java
@@ -0,0 +1,12 @@
+package com.brannanhancock.cnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnnApplication.class, args);
+	}
+}
