99eb36af6c4527eeb320b77652a97c2b6d0b28c5
BrannanH <brannanhancock@gmail.com>
2018-09-09 22:59:11 +0100

    adding initial layout to base page

--- src/main/js/home/BasePage.js
+++ src/main/js/home/BasePage.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   root: {
@@ -14,15 +18,50 @@ const styles = theme => ({
 
 function BasePage(props) {
   const { classes } = props;
-
+  const bull = <span className={classes.bullet}>•</span>;
   return (
+  
     <div>
       <Paper className={classes.root} elevation={1}>
         <Typography variant="headline" component="h3">
-          This is a sheet of paper.
+          Input
         </Typography>
         <Typography component="p">
-          Paper can be used to build surface or other elements for your application.
+          Inputs go here
+        </Typography>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            MNIST
+          </Typography>
+          <Typography component="p">
+            A dataset of handrawn numerical characters 0-9.
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Training: 28x28x60,000
+            <br />
+            Testing: 28x28x10,000
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Select</Button>
+        </CardActions>
+      </Card>
+      </Paper>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="headline" component="h3">
+          Network
+        </Typography>
+        <Typography component="p">
+          Network units go here
+        </Typography>
+      </Paper>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="headline" component="h3">
+          Output
+        </Typography>
+        <Typography component="p">
+          Network outputs go here
         </Typography>
       </Paper>
     </div>
